22133a2f0f2500183214c7afe2af0922e700fdea
manish-bhargava07 <work.manish07@gmail.com>
2025-04-25 12:11:44 +0530

    deplyement issue fixed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => {
   return {
+    base: '/', // 👈 ADD THIS LINE
     plugins: [react()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        '@': resolve(__dirname, './src'),
       },
     },
     server: {
